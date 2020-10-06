a775f8bd0522f65742ab8fcf8706ae1bf43bf96e
xiaoyu0320 <874157551@qq.com>
2020-10-06 09:46:15 +0800

    uni-app 参考

---
+++ pages/index/components/sh-richtext.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="sh-richtext-box mb10" v-if="richText.content"><uni-parser :html="richText.content"></uni-parser></view>
+</template>
+
+<script>
+export default {
+	components: {
+	},
+	data() {
+		return {
+			richText: ''
+		};
+	},
+	computed: {},
+	props: {
+		detail: {
+			type: Object,
+			default: null
+		}
+	},
+	created() {
+		this.detail.id && this.getRichText();
+	},
+	methods: {
+		getRichText() {
+			this.$api('richtext', {
+				id: this.detail.id
+			}).then(res => {
+				this.richText = res.data;
+			});
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+.sh-richtext-box {
+	background: #fff;
+	padding: 30rpx;
+}
+</style>
